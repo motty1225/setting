7d3c4133a34f546bb1f2072a61c9f87264c7222f
hayato <okamoto_hayato@velought.com>
2021-01-13 02:37:38 +0900

    gulp-imageminモジュール変更,gulp-ejs追加

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const {src, dest, series, parallel, watch} = require('gulp')
 const fibers = require('fibers')
 const sass = require('gulp-sass')
 const del = require('del')
-const imagemin = require('imagemin')
+const imagemin = require('gulp-imagemin')
 const browserify = require('browserify')
 const source = require('vinyl-source-stream')
 const babelify = require('babelify')
@@ -10,8 +10,6 @@ const browsersync = require('browser-sync')
 const postcss = require('gulp-postcss')
 const importer = require('postcss-import')
 const autoprefixer = require('autoprefixer')
-const imageminJpeg = require('imagemin-mozjpeg')
-const imageminPng = require('imagemin-pngquant')
 sass.compiler = require('sass')
 
 const path = {
@@ -24,7 +22,18 @@ const clean = () => {
 }
 
 const html = () => {
-  return src(path.src + '*.html')
+  let paths, srcFile
+  if (process.env.NODE_ENV === 'develop') {
+    paths = JSON.parse(fs.readFileSync('./src/config/develop.json'))
+    srcFile = [path.src + '/**/*.ejs', '!' + path.src + '/**/_*.ejs']
+  } else if (process.env.NODE_ENV === 'production') {
+    paths = JSON.parse(fs.readFileSync('./src/config/production.json'))
+    srcFile = [path.src + '/**/*.ejs', '!' + path.src + '/**/_*.ejs', '!' + path.src + '/index.ejs']
+  }
+
+  return src(srcFile)
+    .pipe(ejs(paths))
+    .pipe(rename({ extname: '.html' }))
     .pipe(dest(path.build))
 }
 
@@ -59,11 +68,11 @@ const image = () => {
   return src(path.src + '/**/*.{jpg,jpeg,png,gif,svg}')
     .pipe(
       imagemin([
-        imageminPng({
+        imagemin.optipng({
           quality: [0.65, 0.8],
           speed: 1,
         }),
-        imageminJpeg({
+        imagemin.mozjpeg({
           quality: 80,
         }),
       ])
